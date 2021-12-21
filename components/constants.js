7d89510c8f422c5512d40aa1b43601eb5368f01f
HadiHosseinpour <hadi1998goodboy@gmail.com>
2021-12-21 10:00:35 +0330

    edit

--- components/constants.js
+++ components/constants.js
@@ -1,6 +1,6 @@
-export const apiUrl = "http://localhost:8000";
-export const baseUrl = '';
-//export const baseUrl = '/newtest';
+//export const apiUrl = "http://localhost:8000";
+//export const baseUrl = '';
+export const baseUrl = '/newtest';
 export const userApiUrl = "https://auth.honari.com/api";
 export const searchBaseUrl = "http://search.honari.com";
-//export const apiUrl = 'http://192.168.1.36/shop_laravel/public';
+export const apiUrl = 'https://honari.com/newtestapi';
