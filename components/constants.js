23ce082daf705b2898062ab9d454e8bfe9b0a476
Hadi Hosseinpour <hadi1998goodboy@gmail.com>
2021-12-26 17:18:30 +0330

    edit

--- components/constants.js
+++ components/constants.js
@@ -1,6 +1,6 @@
-export const apiUrl = "http://localhost:8000";
-export const baseUrl = '';
-//export const baseUrl = '/newtest';
+//export const apiUrl = "http://localhost:8000";
+//export const baseUrl = '';
+export const baseUrl = '/newtest';
 export const userApiUrl = "https://auth.honari.com/api";
 export const searchBaseUrl = "http://search.honari.com";
-//export const apiUrl = 'https://honari.com/newtestapi';
+export const apiUrl = 'https://honari.com/newtestapi';
